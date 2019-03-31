40bcd79e9746ba51a977aebb3907c0bfed92966d
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-31 17:17:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     when /cart/
       if @@cart.empty? #responds with empty cart message if the cart is empty
         resp.write "Your cart is empty"
-      else
+      else #responds with a cart list if there is something in there
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
